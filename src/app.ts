88ac07e2eabf011a58bc7a87350fe1fe6ccdebb8
Maria Luiza <maria.lmm.975@gmail.com>
2022-08-09 19:06:46 -0300

    pull request

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ app.use(json());
 
 app.get("/students", async (req: Request, res: Response) => {
   const students = await prisma.student.findMany();
+  console.log(students);
   res.send(students);
 });
 
